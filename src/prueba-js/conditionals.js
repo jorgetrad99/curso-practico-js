29d7f44e5fac5a90077fb457f1e3de9948bffff0
Jorge Trad <jetradl@gmail.com>
2021-06-27 22:34:01 -0500

    Conditionals Test

---
+++ src/prueba-js/conditionals.js
@@ -0,0 +1,28 @@
+const suscriptions = [
+    {
+        'suscriptionType': "Free", 
+        'message': "Solo puedes tomar los cursos gratis"
+    },
+    {
+        'suscriptionType': "Basic",
+         'message': "Puedes tomar casi todos los cursos de Platzi durante un mes"
+    },
+    {
+        'suscriptionType': "Expert", 
+        'message': "Puedes tomar casi todos los cursos de Platzi durante un año"
+    },
+    {
+        'suscriptionType': "Expert+", 
+        'message': "Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año"
+    }
+] 
+
+const mySuscription = "Expert+";
+
+const indexOfSuscription = suscriptions.findIndex(suscription => suscription.suscriptionType === mySuscription);
+
+indexOfSuscription > -1     //Operador Ternario
+    ? console.log(suscriptions[indexOfSuscription].message)
+    : console.error('La suscripción a buscar es inválida, no se pudo encontrar');
+
+
