e1cb28cb6f3db25cca9759485250519825160518
Jorge Trad <jetradl@gmail.com>
2021-06-29 12:49:32 -0500

    Writing some HTML from JavaScript

--- src/2_precios_descuentos/descuentos.js
+++ src/2_precios_descuentos/descuentos.js
@@ -15,3 +15,23 @@ const calcularPrecioConDescuento = (precio, descuento) => {
   porcentajePrecioDescontado,
   precioConDescuento,
 }); */
+
+const onClickButtonPriceDiscount = () => {
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = inputPrice.value;
+
+  const inputDiscount = document.getElementById("InputDiscount");
+  const discountValue = inputDiscount.value;
+
+  const precioConDescuento = calcularPrecioConDescuento(
+    priceValue,
+    discountValue
+  );
+  /* console.log({
+    priceValue,
+    discountValue,
+  }); */
+
+  const resultP = document.getElementById("ResultP");
+  resultP.innerText = `El precio con Descuento es $${precioConDescuento}`;
+};
