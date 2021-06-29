144e5bd5de2f9b8f9472de7d940fae2122664880
Jorge Trad <jetradl@gmail.com>
2021-06-28 21:28:00 -0500

    integration of everything we've been doing

--- src/1_figuras_geométricas/figuras.js
+++ src/1_figuras_geométricas/figuras.js
@@ -1,14 +1,10 @@
 //Código del Cuadrado
-console.group("Cuadrados");
+//console.group("Cuadrados");
 
 const perimetroCuadrado = (lado) => lado * 4; //Arrow Function
 const areaCuadrado = (lado) => lado ** 2; //Otra forma de elevar un número a un exponente, en este caso 2 (al cuadrado)
 
-console.log(`Los lados del cuadrado miden 6cm`);
-console.log(`Calculando el perímetro del cuadrado: ${perimetroCuadrado(6)}cm`);
-console.log(`Calculando el área del cuadrado: ${areaCuadrado(6)}cm^2`);
-
-console.groupEnd();
+//console.groupEnd();
 
 //Código del Triángulo
 console.group("Triángulos");
@@ -19,18 +15,6 @@ const alturaTriangulo = (lado, base) => {
 };
 const perimetroTriangulo = (lado, base) => lado * 2 + base;
 const areaTriangulo = (base, altura) => (base * altura) / 2;
-
-console.log(`Los lados del triángulo miden 6cm, 6cm y 4cm de base`);
-console.log(`Calculando la altura del Triángulo: ${alturaTriangulo(6, 4)}cm`);
-console.log(
-  `Calculando el perímetro del Triángulo: ${perimetroTriangulo(6, 4)}cm`
-);
-console.log(
-  `Calculando el área del Triángulo: ${areaTriangulo(
-    6,
-    alturaTriangulo(6, 4)
-  )}cm`
-);
 
 console.groupEnd();
 
@@ -43,14 +27,5 @@ const PI = Math.PI;
 const diametroCirculo = (radio) => radio * 2;
 const perimetroCirculo = (radio) => diametroCirculo(radio) * PI;
 const areaCirculo = (radio) => radio ** 2 * PI;
-
-console.log(`El radio del Círculo es de ${radioCirculo}cm`);
-console.log(
-  `Calculando el diámetro del Círculo: ${diametroCirculo(radioCirculo)}cm`
-);
-console.log(
-  `Calculando el perímetro del Círculo: ${perimetroCirculo(radioCirculo)}cm`
-);
-console.log(`Calculando el área del Círculo: ${areaCirculo(radioCirculo)}cm^2`);
 
 console.groupEnd();
