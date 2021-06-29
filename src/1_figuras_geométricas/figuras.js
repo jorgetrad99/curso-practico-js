66429780296a1703cf89d5b5d8c883c4a7bbdc92
Jorge Trad <jetradl@gmail.com>
2021-06-28 21:49:43 -0500

    Interacting with JavaScript from HTML

--- src/1_figuras_geométricas/figuras.js
+++ src/1_figuras_geométricas/figuras.js
@@ -1,14 +1,8 @@
 //Código del Cuadrado
-//console.group("Cuadrados");
-
 const perimetroCuadrado = (lado) => lado * 4; //Arrow Function
 const areaCuadrado = (lado) => lado ** 2; //Otra forma de elevar un número a un exponente, en este caso 2 (al cuadrado)
 
-//console.groupEnd();
-
 //Código del Triángulo
-console.group("Triángulos");
-
 const alturaTriangulo = (lado, base) => {
   const angulo = Math.acos(base / 2 / lado); //Obtiene el ángulo formado en radianes
   return lado * Math.sin(angulo); //Devuelve la altura
@@ -16,11 +10,7 @@ const alturaTriangulo = (lado, base) => {
 const perimetroTriangulo = (lado, base) => lado * 2 + base;
 const areaTriangulo = (base, altura) => (base * altura) / 2;
 
-console.groupEnd();
-
 //Código de los Círculos
-console.group("Círculos");
-
 const radioCirculo = 8;
 const PI = Math.PI;
 
@@ -28,4 +18,18 @@ const diametroCirculo = (radio) => radio * 2;
 const perimetroCirculo = (radio) => diametroCirculo(radio) * PI;
 const areaCirculo = (radio) => radio ** 2 * PI;
 
-console.groupEnd();
+//Aquí interactuamos con el HTML
+function calcularPerimetroCuadrado() {
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
+
+  const perimetro = perimetroCuadrado(value);
+  alert(perimetro);
+}
+function calcularAreaCuadrado() {
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
+
+  const perimetro = areaCuadrado(input.value);
+  alert(perimetro);
+}
