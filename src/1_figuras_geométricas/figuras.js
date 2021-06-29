08b237964865bcc0f087bd1fe5a2eb4c0bc88f7c
Jorge Trad <jetradl@gmail.com>
2021-06-29 10:54:43 -0500

    Challenge: Triangle Height

--- src/1_figuras_geométricas/figuras.js
+++ src/1_figuras_geométricas/figuras.js
@@ -3,11 +3,39 @@ const perimetroCuadrado = (lado) => lado * 4; //Arrow Function
 const areaCuadrado = (lado) => lado ** 2; //Otra forma de elevar un número a un exponente, en este caso 2 (al cuadrado)
 
 //Código del Triángulo
-const alturaTriangulo = (lado, base) => {
-  const angulo = Math.acos(base / 2 / lado); //Obtiene el ángulo formado en radianes
-  return lado * Math.sin(angulo); //Devuelve la altura
+const round = (num) => {
+  return Math.round(num * 100) / 100; //Redondeando a 2 decimales
 };
-const perimetroTriangulo = (lado, base) => lado * 2 + base;
+
+const alturaTriangulo = (lado, base) => {
+  medidasTriangulo = {
+    lado: lado,
+    base: base,
+    angulo: Math.acos(base / 2 / lado),
+    altura: lado * Math.sin(Math.acos(base / 2 / lado)), //Devuelve la altura partiendo del ángulo formado en radianes
+  };
+  console.log(`Se ha creado un Triángulo con las siguientes medidas:
+        lados: ${medidasTriangulo.lado}cm
+        base: ${medidasTriangulo.base}cm
+        ángulo: ${round(medidasTriangulo.angulo)}rad
+        altura: ${round(medidasTriangulo.altura)}cm`);
+  return medidasTriangulo;
+};
+
+const isIsosceles = (triangulo) => {
+  return round(triangulo.altura ** 2) ===
+    round(triangulo.lado ** 2 - (triangulo.base / 2) ** 2) //Partiendo del teorema de pitágoras
+    ? true
+    : false;
+};
+
+console.log(
+  isIsosceles(alturaTriangulo(6, 4))
+    ? "Soy Isosceles :D"
+    : "No soy un Triángulo Isosceles :("
+);
+
+const perimetroTriangulo = (lado1, lado2, base) => lado * 2 + base;
 const areaTriangulo = (base, altura) => (base * altura) / 2;
 
 //Código de los Círculos
