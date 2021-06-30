7f484627509dbeacef366b44ff8839345588aaa9
Jorge Trad <jetradl@gmail.com>
2021-06-30 10:56:08 -0500

    Calculating the median in Javascript

--- src/3_promedio_moda_mediana/mediana.js
+++ src/3_promedio_moda_mediana/mediana.js
@@ -1,9 +1,19 @@
-const lista1 = [100, 200, 500, 40000000];
+const lista1 = [10000, 300, 200, 500, 40000000];
+const lista2 = [550, 100, 1300, 50, 700, 720];
 
-const mitadLista1 = parseInt(lista1.length / 2);
+const mitadLista = (lista) => {
+  return parseInt(lista.length / 2);
+};
 
-const esPar = (nuumerosLista) => {
-  return nuumerosLista % 2 === 0 ? true : false;
+const ordenarLista = (lista) => {
+  let numbers = lista;
+  console.log(`Esta es la lista original => [${numbers}]`);
+  numbers.sort((a, b) => a - b);
+  console.log(`Esta es la lista ya ordenada => [${numbers}]`);
+};
+
+const esPar = (numerosLista) => {
+  return numerosLista % 2 === 0 ? true : false;
 };
 
 const calcularMediaAritmetica = (lista) => {
@@ -15,14 +25,26 @@ const calcularMediaAritmetica = (lista) => {
   return promedioLista;
 };
 
-let mediana;
+const calcularMediana = (lista) => {
+  let mediana;
+  ordenarLista(lista);
+  if (esPar(lista.length)) {
+    console.log("La lista contiene un número par de elementos");
+    const elemento1 = lista[mitadLista(lista) - 1];
+    const elemento2 = lista[mitadLista(lista)];
+    const promedioElemento1y2 = calcularMediaAritmetica([elemento1, elemento2]);
 
-if (esPar(lista1.length)) {
-  const elemento1 = lista1[mitadLista1 - 1];
-  const elemento2 = lista1[mitadLista1];
-  const promedioElemento1y2 = calcularMediaAritmetica([elemento1, elemento2]);
+    mediana = promedioElemento1y2;
+  } else {
+    console.log("La lista contiene un número impar de elementos");
+    mediana = lista[mitadLista(lista)];
+  }
+  console.log(`La mediana de tu lista es: ${mediana}`);
+};
 
-  mediana = promedioElemento1y2;
-} else {
-  mediana = lista1[mitadLista1];
-}
+console.group("Lista1");
+calcularMediana(lista1);
+console.groupEnd();
+console.group("Lista2");
+calcularMediana(lista2);
+console.groupEnd();
