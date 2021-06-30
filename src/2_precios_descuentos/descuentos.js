9bfd19216915cc3653e6bf5a43c7ff31e78238db
Jorge Trad <jetradl@gmail.com>
2021-06-29 20:30:09 -0500

    Challenge: Smarter Features

--- src/2_precios_descuentos/descuentos.js
+++ src/2_precios_descuentos/descuentos.js
@@ -1,20 +1,9 @@
-const precioOriginal = 120;
-const descuento = 18;
-
+//Descuentos
 const calcularPrecioConDescuento = (precio, descuento) => {
   const porcentajePrecioDescontado = 100 - descuento;
-  const precioConDescuento =
-    (precioOriginal * porcentajePrecioDescontado) / 100;
-
+  const precioConDescuento = (precio * porcentajePrecioDescontado) / 100;
   return precioConDescuento;
 };
-
-/* console.log({
-  precioOriginal,
-  descuento,
-  porcentajePrecioDescontado,
-  precioConDescuento,
-}); */
 
 const onClickButtonPriceDiscount = () => {
   const inputPrice = document.getElementById("InputPrice");
@@ -27,11 +16,85 @@ const onClickButtonPriceDiscount = () => {
     priceValue,
     discountValue
   );
-  /* console.log({
-    priceValue,
-    discountValue,
-  }); */
-
   const resultP = document.getElementById("ResultP");
   resultP.innerText = `El precio con Descuento es $${precioConDescuento}`;
 };
+
+//Cupones
+const cuponesDisponibles = [
+  ["A523", 25],
+  ["B213", 30],
+  ["C673", 10],
+  ["D328", 50],
+];
+
+const mostrarListaCupones = () => {
+  cuponesDisponibles.forEach((cupon) => {
+    console.log(
+      `Este es el cupón ${cupon[0]} y el descuento es de ${cupon[1]}%`
+    );
+  });
+};
+
+console.group("Lista de Cupones Disponibles");
+mostrarListaCupones();
+console.groupEnd();
+
+const compraProducto1 = {
+  precioOriginal: 750,
+  cupon: "A352",
+};
+
+const compraProducto2 = {
+  precioOriginal: 532,
+  cupon: "B213",
+};
+
+const mostrarCompra = (compra) => {
+  console.log(
+    `Esta compra será originalmente por un total de $${compra.precioOriginal}, pero cuenta con el cupón ${compra.cupon}... ¿Será válido?`
+  );
+};
+
+console.group("Compras");
+mostrarCompra(compraProducto1);
+mostrarCompra(compraProducto2);
+console.groupEnd();
+
+const esCuponValido = (compra) => {
+  const cupon_encontrado = cuponesDisponibles.find((cupon) => {
+    return cupon[0] === compra.cupon;
+  });
+  return cupon_encontrado != null ? true : false;
+};
+
+const findDiscount = (compra) => {
+  if (esCuponValido(compra)) {
+    const cupon_encontrado = cuponesDisponibles.find((cupon) => {
+      return cupon[0] === compra.cupon;
+    });
+    return cupon_encontrado[1];
+  } else {
+    return null;
+  }
+};
+
+const calcularPrecioConCupon = (compra) => {
+  esCuponValido(compra)
+    ? console.log(
+        `El precio con Descuento pasa de ser $${
+          compra.precioOriginal
+        } a ser $${calcularPrecioConDescuento(
+          compra.precioOriginal,
+          findDiscount(compra)
+        )} con un descuento de ${findDiscount(compra)}`
+      )
+    : console.log(
+        `El cupón no es válido, no se aplicarán descuentos... El precio final es de $${compra.precioOriginal}`
+      );
+};
+
+console.group("Validación de Compras");
+calcularPrecioConCupon(compraProducto1);
+calcularPrecioConCupon(compraProducto2);
+console.groupEnd();
